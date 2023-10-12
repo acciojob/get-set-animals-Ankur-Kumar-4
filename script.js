0171c1155ce2f8a81e258f1d7c7ac12635200c58
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-10-12 12:25:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
 //complete this code
 class Animal {
-	constructor(species){
-		this._species = species;
-	}
+	 constructor(species) {
+    this._species = species;
+  }
 
-	get species(){
-		
-		return this._species;
-	}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
+
+  // Method to make a generic sound
+  makeSound() {
+    console.log("The animal makes a sound.");
+  }
 
 	
 }
 
 class Dog extends Animal {
 	
-	bark(){
-		console.log("woof");
-	}
+	constructor(species) {
+    super(species);
+  }
+
+  // Method to make a dog-specific sound
+  bark() {
+    console.log("Woof");
+  }
 }
 
 class Cat extends Animal {
 	
-	purr(){
-		cosole.log("pure");
-	}
+	constructor(species) {
+    super(species);
+  }
+
+  // Method to make a cat-specific sound
+  purr() {
+    console.log("Purr");
+  }
 }
 
 // Do not change the code below this line
